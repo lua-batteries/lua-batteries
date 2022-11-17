e0f7b71d98abdb8b3d4c2cfc3ab3eab9803aa376
clitic <clitic21@gmail.com>
2022-11-17 16:17:15 +0530

    export variables

--- scripts/luajit51.sh
+++ scripts/luajit51.sh
@@ -1,4 +1,4 @@
 BASEDIR=$(dirname $0)
-LUA_PATH="$BASEDIR/../share/lua/5.1/?.lua;$BASEDIR/../share/lua/5.1/?/init.lua;$LUA_PATH"
-LUA_CPATH="$BASEDIR/../lib/lua/5.1/?.so;$BASEDIR/../lib/lua/5.1/loadall.so;$LUA_CPATH"
+export LUA_PATH="$BASEDIR/../share/lua/5.1/?.lua;$BASEDIR/../share/lua/5.1/?/init.lua;$LUA_PATH"
+export LUA_CPATH="$BASEDIR/../lib/lua/5.1/?.so;$BASEDIR/../lib/lua/5.1/loadall.so;$LUA_CPATH"
 exec "$BASEDIR/luajit" "$@"
--- subprojects/luarocks/scripts/luarocks-admin.sh
+++ subprojects/luarocks/scripts/luarocks-admin.sh
@@ -1,6 +1,6 @@
 BASEDIR=$(dirname $0)
-LUA_PATH="$BASEDIR/../share/lua/5.4/?.lua;$BASEDIR/../share/lua/5.4/?/init.lua;$LUA_PATH"
-LUA_CPATH="$BASEDIR/../lib/lua/5.4/?.so;$BASEDIR/../lib/lua/5.4/loadall.so;$LUA_CPATH"
-LUAROCKS_SYSCONFDIR="$BASEDIR/../share/lua"
-exec "$BASEDIR/lua" "$BASEDIR/../share/lua/bin/luarocks.lua" config --scope system home_tree "$BASEDIR/.." > /dev/null
+export LUA_PATH="$BASEDIR/../share/lua/5.4/?.lua;$BASEDIR/../share/lua/5.4/?/init.lua;$LUA_PATH"
+export LUA_CPATH="$BASEDIR/../lib/lua/5.4/?.so;$BASEDIR/../lib/lua/5.4/loadall.so;$LUA_CPATH"
+export LUAROCKS_SYSCONFDIR="$BASEDIR/../share/lua"
+# exec "$BASEDIR/lua" "$BASEDIR/../share/lua/bin/luarocks.lua" config --scope system home_tree "$BASEDIR/.." > /dev/null
 exec "$BASEDIR/lua" "$BASEDIR/../share/lua/bin/luarocks-admin.lua" "$@"
