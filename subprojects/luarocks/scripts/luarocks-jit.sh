3a65ae828535ed98d1dc8bc067d0166840fe32a8
clitic <clitic21@gmail.com>
2022-11-18 11:53:00 +0530

    added ltui support

--- subprojects/luarocks/scripts/luarocks-jit.sh
+++ subprojects/luarocks/scripts/luarocks-jit.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname $0)
-export LUA_PATH="$BASEDIR/../share/lua/5.1/?.lua;$BASEDIR/../share/lua/5.1/?/init.lua;$LUA_PATH"
+export LUA_PATH="$BASEDIR/../share/luajit-2.0.5/?.lua;$BASEDIR/../share/lua/5.1/?.lua;$BASEDIR/../share/lua/5.1/?/init.lua;$LUA_PATH"
 export LUA_CPATH="$BASEDIR/../lib/lua/5.1/?.so;$BASEDIR/../lib/lua/5.1/loadall.so;$LUA_CPATH"
 export LUAROCKS_SYSCONFDIR="$BASEDIR/../share/lua"
 "$BASEDIR/luajit" "$BASEDIR/../share/lua/bin/luarocks.lua" config --scope system home_tree "$BASEDIR/.." > /dev/null
