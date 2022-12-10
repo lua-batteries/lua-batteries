3aa5c7cb77274cb1a810a4429b477077ba408963
clitic <clitic21@gmail.com>
2022-12-10 10:56:34 +0530

    updated paths

--- lua.sh
+++ lua.sh
@@ -1,4 +1,4 @@
 BASEDIR=$(dirname $0)
-export LUA_PATH="$BASEDIR/../share/lua/@LUA_VER@/?.lua;$BASEDIR/../share/lua/@LUA_VER@/?/init.lua;$LUA_PATH"
+export LUA_PATH="@JIT@$BASEDIR/../share/lua/@LUA_VER@/?.lua;$BASEDIR/../share/lua/@LUA_VER@/?/init.lua;$LUA_PATH"
 export LUA_CPATH="$BASEDIR/../lib/lua/@LUA_VER@/?.so;$BASEDIR/../lib/lua/@LUA_VER@/loadall.so;$LUA_CPATH"
 exec "$BASEDIR/@BIN@" "$@"
